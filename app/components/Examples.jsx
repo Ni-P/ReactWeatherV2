d6604a1c6e3d999a0a39c0f01c87f3a6f5a37da9
Niko Pinnis <niko.pinnis@outlook.com>
2017-08-16 19:02:24 +0300

    Add more examples and update About

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,18 +1,44 @@
-
 var React = require('react');
-var {Link} = require('react-router');
+var { Link } = require('react-router');
 
-var Examples = (props) => {
+var Examples = props => {
   return (
     <die>
-      <h1 className='text-center page-title'>Examples</h1>
+      <h1 className="text-center page-title">Examples</h1>
       <p>Here's a few locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Kemi'>Kemi, FI</Link>
+          <Link to="/?location=Helsinki">Helsinki, FI</Link>
         </li>
         <li>
-          <Link to='/?location=Athens'>Athens, GR</Link>
+          <Link to="/?location=Athens">Athens, GR</Link>
+        </li>
+        <li>
+          <Link to="/?location=London">London, UK</Link>
+        </li>
+        <li>
+          <Link to="/?location=New York">New York, US</Link>
+        </li>
+        <li>
+          <Link to="/?location=Paris">Paris, FR</Link>
+        </li>
+        <li>
+          <Link to="/?location=Moscow">Moscow, RU</Link>
+        </li>
+        <li>
+          <Link to="/?location=Sydney">Sydney, AU</Link>
+        </li>
+        <li>
+          <Link to="/?location=Tokyo">Tokyo, JP</Link>
+        </li>
+        <li>
+          <Link to="/?location=Berlin">Berlin, DE</Link>
+        </li>
+        <li>
+          <Link to="/?location=Stockholm">Stockholm, SE</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio de Janeiro">Rio de Janeiro, BR</Link>
         </li>
       </ol>
     </die>
